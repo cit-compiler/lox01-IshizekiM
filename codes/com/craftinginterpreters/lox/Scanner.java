ad8ce7bc3b6f9cd664244430db32c58d6d9e6428
cima19012 <cima19012@g.nihon-u.ac.jp>
2025-12-02 10:28:39 +0900

    add string method

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -79,10 +79,29 @@ public class Scanner {
                 line++;
                 break;
 
+            case '=': string(); break;
+
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private void string(){
+        while(peek() != '"' && !isAtEnd()){
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd()){
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance();
+
+        String value =source.substring(start + 1, current - 1);
+        return;
     }
 
     private boolean match(char expected){
