c44b74e642fda04923dc8009e6db1a879f9c91ec
cima19012 <cima19012@g.nihon-u.ac.jp>
2026-01-13 10:29:27 +0900

    update Interpreter.java Lox.java add Expr.java

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox{
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException{
         if(args.length > 1){
             System.out.println("Usage: jlox [script]");
@@ -25,9 +27,9 @@ public class Lox{
         byte[] bytes =Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
 
-        if(hadError) {
-            System.exit(65);
-        }
+        if(hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
+
     }
 
     private static void runPrompt() throws IOException{
@@ -55,6 +57,7 @@ public class Lox{
 
         // Stop if there was a syntax error.
         if (hadError) return;
+        interpreter.interpret(expression);
 
         System.out.println(new AstPrinter().print(expression));
 
@@ -62,6 +65,12 @@ public class Lox{
 
     static void error(int line, String message){
         report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+            "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String mhere, String message){
